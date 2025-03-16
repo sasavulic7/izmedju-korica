abb2af3f1c7fc0d40d89ddd5d9582f55e0a6f49a
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2025-03-16 18:50:53 +0100

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://izmedju-korica.vercel.app/api"
 });
 
 // Interceptor za dodavanje tokena u zahteve
