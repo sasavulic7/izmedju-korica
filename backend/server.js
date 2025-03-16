96f4d9a852b58800b4c68bea57762b6d85adeecc
sasavulic7 <e23sasavulic@gmail.com>
2025-03-16 18:21:18 +0100

    feat: test

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,9 @@ app.use("/api", contactRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://e23sasavulic:sasavulic123@bookblogcluster.odtgx.mongodb.net/?retryWrites=true&w=majority&appName=BookBlogCluster"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
